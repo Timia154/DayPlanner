fa2b73d309cdcaa1f9124989b867b057e2434776
Timia 154 <miamia7716@icloud.com>
2021-04-09 16:58:00 -0400

    tried to add time blocks

---
+++ Develop/day.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function ()
+
+
+
+$ var currentDate = moment().format("dddd, MMMM Do YYYY");
+("#currentDay").text(currentDate);
+var currentTime = moment().format("H");
+
+var newDiv = ("<div>");
+        var newSpan = ("<span>");
+        var newTextInput = ("<input>");
+        var newBtn = ("<button>");
+
+
+        $("#hour8 .description").val(localStorage.getItem("hour8"));
+        $("#hour9 .description").val(localStorage.getItem("hour9"));
+        $("#hour10 .description").val(localStorage.getItem("hour10"));
+        $("#hour11 .description").val(localStorage.getItem("hour11"));
+        $("#hour12 .description").val(localStorage.getItem("hour12"));
+        $("#hour13 .description").val(localStorage.getItem("hour13"));
+        $("#hour14 .description").val(localStorage.getItem("hour14"));
+        $("#hour15 .description").val(localStorage.getItem("hour15"));
+        $("#hour16 .description").val(localStorage.getItem("hour16"));
+        $("#hour17 .description").val(localStorage.getItem("hour17"));
+        
